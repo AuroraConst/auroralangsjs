570e199cc8d1cf64bdce3a860f8d0a4bd6617426
saki-aurora <skaushi1@lakeheadu.ca>
2025-04-08 13:43:07 -0400

    feat: add implementation for issue coordinate similar to order coordinate

--- src/main/scala/docere/sjsast/IssueCoordinate.scala
+++ src/main/scala/docere/sjsast/IssueCoordinate.scala
@@ -1,13 +1,16 @@
 package docere.sjsast
 
 
-case class IssueCoordinate (name:String) extends SjsNode :
+case class IssueCoordinate (name:String, ref: Set[RefCoordinate]) extends SjsNode :
 
-  def merge(i: IssueCoordinate): IssueCoordinate = i
+  def merge(i: IssueCoordinate): IssueCoordinate = 
+    val result = ref |+| i.ref
+    IssueCoordinate(name, result)
 
-  override def merge(p: SjsNode): SjsNode = merge(p.asInstanceOf[IssueCoordinate])
-
+  override def merge(p: SjsNode): SjsNode =
+     merge(p.asInstanceOf[IssueCoordinate])
 
 object IssueCoordinate :
   def apply(i: GenAst.IssueCoordinate): IssueCoordinate = 
-    IssueCoordinate(i.name)
+    val resultRef = i.refs.toList.map{r =>  RefCoordinate(r.$refText)}.toSet
+    IssueCoordinate(i.name, resultRef)
