766b325976db7ac8b6134d6341df77f7cfaea287
Samia Ishaque <145932439+SamiaIshaqML@users.noreply.github.com>
2025-04-14 11:52:42 -0400

    changes to clinicalcord, constructors and ngc file for addings refs/narrative

--- src/main/scala/docere/sjsast/ClinicalCoordinate.scala
+++ src/main/scala/docere/sjsast/ClinicalCoordinate.scala
@@ -1,15 +1,17 @@
 package docere.sjsast
 
-case class ClinicalCoordinate (name :String, narrative:Set[Narrative]=Set.empty) extends SjsNode:
+case class ClinicalCoordinate (name :String, narrative:Set[Narrative]=Set.empty, refs: Set[RefCoordinate] = Set.empty) extends SjsNode:
 
   def merge(cc:ClinicalCoordinate):ClinicalCoordinate = 
     val narratives = narrative |+| cc.narrative
-    ClinicalCoordinate(name, narratives)
+    val result = refs |+| cc.refs
+    ClinicalCoordinate(name, narratives, result)
   override def merge(p: SjsNode): SjsNode = 
     merge(p.asInstanceOf[ClinicalCoordinate])
 
 object ClinicalCoordinate{
   def apply (c: GenAst.ClinicalCoordinate): ClinicalCoordinate = 
     val narratives = c.narrative.toList.map{n =>  Narrative(n.name)}.toSet
-    ClinicalCoordinate(c.name, narratives)
+    val x = c.refs.toList.map { r => RefCoordinate(r.$refText) }.toSet
+    ClinicalCoordinate(c.name, narratives, x)
 }
--- src/main/scala/docere/sjsast/NGC.scala
+++ src/main/scala/docere/sjsast/NGC.scala
@@ -1,14 +1,16 @@
 package docere.sjsast
 // type CV = ClinicalCoordinate | ClinicalValue
 
-case class NGC (name:String, ccoords:Set[ClinicalCoordinate], narrative:Set[Narrative]=Set.empty)  extends SjsNode :
+case class NGC (name:String, ccoords:Set[ClinicalCoordinate], narrative:Set[Narrative]=Set.empty,refs: Set[RefCoordinate] = Set.empty )  extends SjsNode :
   def merge(n:NGC):NGC = 
     val narratives = narrative |+| n.narrative
-    NGC(name,ccoords merge n.ccoords, narratives)
+    val result = refs |+| n.refs
+    NGC(name,ccoords merge n.ccoords, narratives, refs)
   override def merge(p: SjsNode): SjsNode = merge(p.asInstanceOf[NGC])
 
 object NGC:
   def apply(n: GenAst.NGC): NGC = 
     val narratives = n.narrative.toList.map{n =>  Narrative(n.name)}.toSet
     val cc = n.coord.toList.map{ c => ClinicalCoordinate(c.asInstanceOf[GenAst.ClinicalCoordinate])}.toSet
-    NGC(n.name, cc, narratives)
+    val x = n.refs.toList.map { r => RefCoordinate(r.$refText) }.toSet
+    NGC(n.name, cc, narratives, x)
